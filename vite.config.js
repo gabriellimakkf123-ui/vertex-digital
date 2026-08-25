a2584454a1ab491b1c19a80ea8798b2a83963b5e
Gabriel Lima <gabriellimakkf123@gmail.com>
2026-08-25 19:22:19 -0300

    feat: adicionar plano-monalisa e remover arquivos exseguros

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        monalisa: resolve(__dirname, 'plano-monalisa.html')
+      }
+    }
   }
 });
